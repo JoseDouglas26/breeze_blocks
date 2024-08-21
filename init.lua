b32dda706b00d8475a499a57eec4a3afb7fc109a
JoseDouglas26 <josedouglas20002014@gmail.com>
2024-08-21 19:34:20 -0300

    Placement mechanic

--- init.lua
+++ init.lua
@@ -9,6 +9,18 @@ local function make_invimg(texture, model)
     return texture.."^breeze_blocks_mask_"..model..".png^[makealpha:250,130,50"
 end
 
+local function place_node(itemstack, placer, pointed_thing)
+    if pointed_thing.type ~= "node" or not placer then
+        return
+    end
+
+    if pointed_thing.above.y == pointed_thing.under.y then
+        return minetest.item_place(itemstack, placer, pointed_thing, 4)
+    else
+        return minetest.rotate_node(itemstack, placer, pointed_thing)
+    end
+end
+
 for _, model in pairs(models) do
     for color, defs in pairs(mcl_dyes.colors) do
         local texture = "mcl_colorblocks_concrete_"..color..".png"
@@ -25,7 +37,7 @@ for _, model in pairs(models) do
             paramtype2 = "facedir",
             drawtype = "mesh",
             mesh = "breeze_blocks_"..model..".obj",
-            on_place = minetest.rotate_node,
+            on_place = place_node,
             sounds = mcl_sounds.node_sound_stone_defaults(),
             _mcl_blast_resistance = 1.8,
             _mcl_hardness = 1.8,
