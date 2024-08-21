5708a5ab3cab4134cadb3064d9e1c25ca92f5224
JoseDouglas26 <josedouglas20002014@gmail.com>
2024-08-21 17:09:58 -0300

    Breeze blocks

---
+++ init.lua
@@ -0,0 +1,34 @@
+local S = minetest.get_translator("mcl_breeze_blocks")
+local models = { "rods", "eye", "rhomb", "cross" }
+
+local function make_desc(name)
+	return name:sub(1,1):upper()..name:sub(2)
+end
+
+local function make_invimg(texture, model)
+    return texture.."^mcl_breeze_blocks_mask_"..model..".png^[makealpha:250,130,50"
+end
+
+for _, model in pairs(models) do
+    for color, defs in pairs(mcl_dyes.colors) do
+        local texture = "mcl_colorblocks_concrete_"..color..".png"
+
+        minetest.register_node("mcl_breeze_blocks:"..model.."_"..color, {
+            description = S("@1 @2 Breeze Block", defs.readable_name, make_desc(model)),
+            groups = { pickaxey = 1 },
+            collision_box = { type = "fixed", fixed = { -0.5, -0.5, -0.125, 0.5, 0.5, 0.125 } },
+            selection_box = { type = "fixed", fixed = { -0.5, -0.5, -0.125, 0.5, 0.5, 0.125 } },
+            sunlight_propagates = true,
+            inventory_image = make_invimg(texture, model),
+            tiles = { texture },
+            paramtype = "light",
+            paramtype2 = "facedir",
+            drawtype = "mesh",
+            mesh = "mcl_breeze_blocks_"..model..".obj",
+            on_place = minetest.rotate_node,
+            sounds = mcl_sounds.node_sound_stone_defaults(),
+            _mcl_blast_resistance = 1.8,
+            _mcl_hardness = 1.8
+        })
+    end
+end
